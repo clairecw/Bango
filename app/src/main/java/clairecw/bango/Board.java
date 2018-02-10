f0ce66e7fd157bb4e47456a709ba38f50a35a49b
claire <basketballclaire@gmail.com>
2018-02-09 19:59:05 -0500

    test

---
+++ app/src/main/java/clairecw/bango/Board.java
@@ -0,0 +1,16 @@
+package clairecw.bango;
+
+/**
+ * Created by admin on 2/9/18.
+ */
+
+
+
+public class Board {
+
+    //String id;
+    //WinConfig wc;
+    //int size;       // board has size x size dimensions
+    boolean[][] marked;
+    String[][] squares;
+}
